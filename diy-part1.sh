1da8c6b1c4db96f373979341f591487fd7abc9fa
tsgdgszjj-sketch <tsgdgszjj@gmail.com>
2026-04-07 20:32:31 +0800

    fix: remove third-party feeds, use git clone in diy-part2 instead

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-# DIY Part 1 - 在 feeds update 之前执行
-# 功能：添加第三方 feed 源（Passwall）
-
-set -e
-
-echo ">>> [DIY-1] 添加 Passwall feeds..."
-# 注意：OpenWrt feeds 脚本不支持 ;branch 语法，直接用仓库 URL
-cat >> feeds.conf.default << 'FEEDS'
-src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git
-src-git passwall https://github.com/xiaorouji/openwrt-passwall.git
-FEEDS
-
-echo ">>> [DIY-1] feeds.conf.default 当前内容："
+# DIY Part 1 - feeds update 之前执行
+# Passwall 和 OpenClash 通过 diy-part2.sh 的 git clone 方式引入，无需额外 feed
+echo ">>> [DIY-1] 使用默认 feeds，第三方包通过 git clone 引入"
 cat feeds.conf.default
 echo ">>> [DIY-1] 完成！"
